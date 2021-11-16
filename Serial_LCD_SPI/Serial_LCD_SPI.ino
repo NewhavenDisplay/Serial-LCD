6dcb66a991ef9566f635171e8554e8b8b2815755
NHD-DOMAIN\CJohnson <cjohnson@newhavendisplay.com>
2021-11-16 14:36:48 -0600

    Write works.

--- Serial_LCD_SPI/Serial_LCD_SPI.ino
+++ Serial_LCD_SPI/Serial_LCD_SPI.ino
@@ -27,138 +27,63 @@
 #define EXTENDED_INSTRUCTION_TABLE_ON 0x01
 #define EXTENDED_INSTRUCTION_TABLE_OFF 0x00
 
-uint8_t _SCL;
-uint8_t _RS;
-uint8_t _SDI;
-uint8_t _CS;
-uint8_t _RESET;
-
-uint8_t contrast = 0x20;
-
-uint8_t customChar[] = {
-  0b00011111,
-  0b00001010,
-  0b00001010,
-  0b00001010,
-  0b00001010,
-  0b00011111,
-  0b00000000,
-  0b00000000
-};
+uint8_t _SCL; // 5
+uint8_t _SDI; // 4
+uint8_t _CS; // 3
 
 void setup() 
 {
   Serial.begin(9600);
 
-  initCOG(7, 6, 5, 4, 3);
-
-  setCustomCharacter();
-  write(0x00);
+  initLCD(5, 4, 3);
+  write('A');
   
 }
 
 void loop() 
 {
-  /*
-  setContrast(contrast);
-  delay(300);
-  contrast--;
 
-  if(contrast < 0x10)
-  {
-    contrast = 0x20;
-  }
-  */
 }
 
-void initCOG(uint8_t SCL, uint8_t RS, uint8_t SDI, uint8_t CS ,uint8_t RESET)
+void initLCD(uint8_t SCL, uint8_t SDI, uint8_t CS)
 {
   _SCL = SCL;
-  _RS = RS;
   _SDI = SDI;
   _CS = CS;
-  _RESET = RESET;
 
   pinMode(CS, OUTPUT);
   pinMode(SCL, OUTPUT);
-  pinMode(RS, OUTPUT);
   pinMode(SDI, OUTPUT);
-  pinMode(RESET, OUTPUT);
 
   digitalWrite(CS, HIGH);
   digitalWrite(SCL, HIGH);
-  digitalWrite(RESET, HIGH);
 
-  reset();
+  delay(500);
 
-  command(0x30); // wakeup
-  delay(2);
-  command(0x30); // wakeup
-  command(0x30); // wakeup
-  command(0x39); // function set
-  command(0x14); // internal osc frequency
-  command(0x56); // power control
-  command(0x6D); // follower control
-  command(0x70); // contrast
-  command(0x0C); // display ON
-  command(0x06); // entry mode
-  command(0x01); // clear screen
-  delay(10);
-}
-
-void reset()
-{
-  digitalWrite(_RESET, LOW);
-  delay(2);
-  digitalWrite(_RESET, HIGH);
-  delay(20);
+  // Init
+  displayON();
+  clearScreen();
 }
 
 void setCS()
 {
   digitalWrite(_CS, HIGH);
+  delay(1);
 }
 
 void clearCS()
 {
   digitalWrite(_CS, LOW);
-}
-
-void setCommand()
-{
-  // Write RS
-  digitalWrite(_RS, LOW);
-}
-
-void setData()
-{
-  // Write RS
-  digitalWrite(_RS, HIGH);
-}
-
-void command(uint8_t data)
-{
-  clearCS();
   delay(1);
-
-  setCommand();
-  
-  putData(data);
-  
-  setCS();
 }
 
 void write(uint8_t data)
 {
   clearCS();
-  delay(1);
-
-  setData();
   
   putData(data);
   
   setCS();
-  delay(1);
 }
 
 void putData(uint8_t data)
@@ -174,53 +99,51 @@ void putData(uint8_t data)
   }
 }
 
-void functionSet(uint8_t interfaceData, uint8_t lines, uint8_t doubleHeight, uint8_t instructionTable)
+void prefix()
 {
-  command(0x20 | interfaceData | lines | doubleHeight | instructionTable);
+  write(0xFE);
+}
+
+void displayON()
+{
+  prefix();
+  write(0x41);
+}
+
+void displayOFF()
+{
+  prefix();
+  write(0x42);
+}
+
+void setCursor(uint8_t position)
+{
+  prefix();
+  write(position);
+}
+
+void home()
+{
+  prefix();
+  write(0x46);
+}
+
+void clearScreen()
+{
+  prefix();
+  write(0x51);
 }
 
 void setContrast(uint8_t contrast)
 {
-  uint8_t contrastLower = contrast & 0x0F;
-  uint8_t contrastUpper = (contrast >> 4) & 0x03;
-
-  functionSet(DATA_8BIT, LINES_1, DOUBLEHEIGHT_ON, EXTENDED_INSTRUCTION_TABLE_ON);
-  command(0x50 | 0x04 | contrastUpper); // Power control 
-  command(0x70 | contrastLower); // Set contast
+  prefix();
+  write(0x52);
+  write(contrast);
 }
 
-void setDoubleHeight()
+void setBrightness(uint8_t brightness)
 {
-  functionSet(DATA_8BIT, LINES_1, DOUBLEHEIGHT_ON, EXTENDED_INSTRUCTION_TABLE_OFF);
-  
-}
-
-void setCGRAM(uint8_t address)
-{
-  command(0x40 | (address & 0x3F));
-}
-
-void setCustomCharacter()
-{
-  
-  functionSet(DATA_8BIT, LINES_2, DOUBLEHEIGHT_OFF, EXTENDED_INSTRUCTION_TABLE_OFF);
-  setCGRAM(0);
-  for(int i = 0; i < 8; i++)
-  {
-    
-    write(customChar[i]);
-  }
-  // Set CGRAM address to 0x00
-  // Write byte of character data to CGRAM
-  delay(1000);
-  functionSet(DATA_8BIT, LINES_2, DOUBLEHEIGHT_OFF, EXTENDED_INSTRUCTION_TABLE_OFF);
-  command(0x01);
-  delay(2);
-  command(0x02);
-  delay(5);
-}
-
-void drawCustomCharacter()
-{
-  
+  prefix();
+  write(0x53);
+  write(brightness);
 }
